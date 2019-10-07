8d15fdda8944ac578144c6f4d659365417a39069
Robin Wenzel <info@robin-wenzel.de>
2019-10-07 20:33:40 +0200

    Add Commandline Flags

--- main.go
+++ main.go
@@ -1,32 +1,56 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"os"
 
 	tmdb "github.com/cyruzin/golang-tmdb"
 	cmd "github.com/cyruzin/trailer/cmd/trailer"
-	"github.com/joho/godotenv"
 )
 
-func init() {
-	if err := godotenv.Load(); err != nil {
-		log.Print("No .env file found")
-	}
-}
-
 func main() {
-	apiKey, _ := os.LookupEnv("TMDB_KEY")
 
-	client, err := tmdb.Init(apiKey)
+	// Declare cli flags
+	useTMDB := flag.Bool("tmdb", false, "Use TheMovieDB API")
+	searchMovie := flag.Bool("movie", false, "Search for Movies")
+	searchTV := flag.Bool("tv", false, "Search for TV Shows")
+	apiKey := flag.String("apiKey", "", "API key for The Movie DB")
+	help := flag.Bool("help", false, "Shows the Help")
+	flag.Parse()
+
+	if *help {
+		fmt.Println("usage:")
+		fmt.Println("trailer -tmdb -apiKey=<yourApiKey> -movie Batman")
+		fmt.Println("Available Flags:")
+		flag.PrintDefaults()
+		return
+	}
+
+	log.Printf("Using with Flags: -tmdb=%t, -movie=%t, -tv=%t, -apiKey=%s", *useTMDB, *searchMovie, *searchTV, *apiKey)
+
+	if *apiKey == "" {
+		apiKey, ok := os.LookupEnv("TMDB_KEY")
+		if !ok {
+			log.Println("TMDB api key is not")
+			return
+		}
+		if apiKey == "" {
+			log.Println("TMDB api key is empty")
+			return
+		}
+	}
+
+	client, err := tmdb.Init(*apiKey)
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
-	trailer := cmd.NewTMDBClient(client)
+	tClient := cmd.NewTMDBClient(client)
 
-	if err := trailer.RootCmd().Execute(); err != nil {
+	if err := tClient.RootCmd().Execute(); err != nil {
 		log.Println(err)
 		os.Exit(1)
 	}
